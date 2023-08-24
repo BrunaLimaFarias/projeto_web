fc45ec5d4e18065922c78afe1ddd50a46fa60fbf
Bruna Lima Farias <burunalfarias@gmail.com>
2023-08-24 20:56:58 -0300

    comunicacao cliente servidor

---
+++ aula4/js/index.js
@@ -0,0 +1,14 @@
+
+function gravar(){
+
+    var form = document.getElementById('formCadastro');
+    var dados = new FormData(form);
+
+
+
+    fetch("php/gravar.php", {
+        method: "POST",
+        body: dados
+    })
+
+}
