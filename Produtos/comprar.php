4491f25ecaf1c447c01999e1293817a0a156fbae
Bruna Lima Farias <burunalfarias@gmail.com>
2023-11-09 08:18:38 -0300

    comprar

--- Produtos/comprar.php
+++ Produtos/comprar.php
@@ -1,9 +1,10 @@
 <?php
+    $id_produto = $_POST["id_produto"];
     $quantidade = $_POST["quantidade"];
 
     $con = mysqli_connect("localhost:3306", "root", "root", "projeto_web");
 
-    $query = "INSERT INTO carrinho (quantidade) VALUES ('$quantidade')";
+    $query = "INSERT INTO carrinho (id_produto, quantidade VALUES ($id_produto, $quantidade)";
     
 
     mysqli_query($con, $query);
